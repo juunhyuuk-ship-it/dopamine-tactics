4942770f28dda4ab76f31d90698dcfb747740d58
juunhyuuk-ship-it <juunhyuuk@gmail.com>
2026-06-14 15:33:01 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // 아직 Google Sheets를 연결하지 않았다면 빈 값으로 두세요.
 // 빈 값이면 같은 폴더의 data.csv를 읽습니다.
 
-window.DOPAMINE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTVizAixUdC5uyRDGL_z7lWejOX1rDnmBYFYe2lX6xRJa3K0piqGcbsLGnBDIuaHQ/pub?gid=1068477996&single=true&output=csv";
+window.DOPAMINE_SHEET_CSV_URL = "";
